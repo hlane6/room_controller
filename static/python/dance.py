d62c36f2326d4b50895a3a05afd195acabd4e4ec
Harry Lane <harryc.lane@gmail.com>
2016-01-06 15:48:03 -0500

    implemented beat detector in python, was more confusing

--- static/python/dance.py
+++ static/python/dance.py
@@ -1,25 +1,33 @@
 from __future__ import print_function
 import numpy
+import audio
 
 points = []
 
-def dance(data):
-    freq_data = numpy.fft.fft(data)
+def dance(length, data):
     bass = 0
+    k = numpy.arange(length)            # start with [1,2,3...]
+    T = length / float(audio.RATE)      # get inverse of sample rate
+    frq = k / T                         # frq = [1/T, 2/T, 3/T, ...]
+    frq = frq[range(length / 4)]        # only get first n / 4 freqs
 
-    for freq, index in enumerate(freq_data.real):
-        if index < 3:
-            bass += freq
+    fourier = numpy.fft.fft(data) / length    # apply fft and normalize
+    fourier = abs(fourier[range(length / 4)])
+     
+    for i in range(3):
+        bass += fourier.real[i]
 
     points.append(bass)
 
-    if len(points) > 5:
+    if len(points) > 43:
         del points[0]
 
     dy = (points[-1] - points[0]) / len(points)
+    average_bass = numpy.average(points)
 
-    if (dy > 4):
-        print(dy, points)
+    if (bass > average_bass * 4):
+        print("Average: {0}\nBass: {1}\nBass/Average: {2}\n".format(average_bass,
+                bass, bass / average_bass))
         return True
 
     return False
--- static/python/run.py
+++ static/python/run.py
@@ -1,21 +1,46 @@
 from dance import dance
 import audio
 import numpy
+import threading
 
 recent = False
 timeout = 0
 
+def update(i):
+    length, data = audio.inp.read()
+    signal = numpy.fromstring(data, dtype='int16')
+
+    fourier = numpy.fft.fft(signal) / length    # apply fft and normalize
+    fourier = fourier[range(length / 4)]
+
+    line.set_ydata(abs(fourier.real))
+    return line,
+
 if __name__ == '__main__':
     while True:
-        l, data = audio.inp.read()
+        length, data = audio.inp.read()
         a = numpy.fromstring(data, dtype='int16')
-        
+    
         if not recent:
-            if dance(a):
+            if dance(length, a):
                 timeout = 0
                 recent = True
         else:
-            if timeout < audio.RATE * .3:
+            if timeout < audio.RATE * .4:
                 timeout += audio.CHUNK
             else:
                 recent = False
+
+    #import matplotlib.pyplot as plt
+    #import matplotlib.animation as animation
+
+    #length, data = audio.inp.read()
+    #signal = numpy.fromstring(data, dtype='int16')
+
+    #fig, ax = plt.subplots()
+    #plt.axis([0, 12000, 0, 1000])
+    #line, = ax.plot(frq, abs(fourier))
+
+    #ani = animation.FuncAnimation(fig, update, interval = 20)
+
+    #plt.show()
